24784030272d02856262a9617d224accc90a52cc
Ste Wootten <stewootten@gmail.com>
2015-10-04 22:18:59 +0100

    REFACTOR moved logic into convenience method to tidy up setter

--- lib/job_list.rb
+++ lib/job_list.rb
@@ -17,16 +17,8 @@ class JobList
 			job_and_dependancy = job.gsub(/\s+/, "").split("=>", -1)
 			array_of_job_objects << Job.new(job_and_dependancy[0], job_and_dependancy[1])
 		end
-
-		jobs_with_dependancy = array_of_job_objects.select { |job| job.dependancy_id != "" }
-
-		jobs_with_dependancy.each do |dependant_job|
-			dependacy_index = array_of_job_objects.find_index {|job| dependant_job.dependancy_id == job.job_id}
-			dependant_index = array_of_job_objects.find_index {|job| dependant_job.job_id == job.job_id}
-
-			array_of_job_objects.insert(dependant_index, array_of_job_objects.delete_at(dependacy_index)) if dependacy_index > dependant_index
-		end
-		@jobs = array_of_job_objects
+		
+		@jobs = order_jobs(array_of_job_objects)
 
 	rescue SelfDependancyError => e
 		puts e.message
@@ -36,6 +28,21 @@ class JobList
 	def jobs
 		return "" if @jobs.length == 0
 		@jobs.collect { |job| job.job_id }.join("")
+	end
+
+	private
+
+	def order_jobs(array_of_job_objects)
+		jobs_with_dependancy = array_of_job_objects.select { |job| job.dependancy_id != "" }
+
+		jobs_with_dependancy.each do |dependant_job|
+			dependacy_index = array_of_job_objects.find_index {|job| dependant_job.dependancy_id == job.job_id}
+			dependant_index = array_of_job_objects.find_index {|job| dependant_job.job_id == job.job_id}
+
+			array_of_job_objects.insert(dependant_index, array_of_job_objects.delete_at(dependacy_index)) if dependacy_index > dependant_index
+		end
+
+		return array_of_job_objects
 	end
 
 end
